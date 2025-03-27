7beeb37f6e5586b350f22570bc01958155dca954
Hector Zapata <hezapata@falabella.cl>
2025-03-27 20:21:43 -0300

    Test pass

--- test/app.test.js
+++ test/app.test.js
@@ -5,14 +5,14 @@ const app = require('../app');
 
 describe('API Tests', () => {
   it('should return a list of tasks', async () => {
-    const res = await request(app).get('/users');
+    const res = await request(app).get('/tasks');
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveLength(2);
   });
 
   it('should return a single task', async () => {
-    const res = await request(app).get('/users/1');
+    const res = await request(app).get('/tasks/1');
     expect(res.statusCode).toEqual(200);
-    expect(res.body.name).toEqual('Alice');
+    expect(res.body.name).toEqual('Task 1');
   });
 });
